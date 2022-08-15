d67aa95b0000f9704ca9145d2d9ec8fc79d15159
Brandon Ekoto <be_developer@bitbucket.com>
2022-08-15 06:59:11 +0100

    rrr

--- esu/dist/dto/centre.dto.d.ts
+++
@@ -1,9 +0,0 @@
-import { Centre } from "src/entity/centre";
-import { EntityTransformer } from "src/util/entity_transformer";
-export declare class CentreDto implements EntityTransformer<Centre> {
-    toEntity(): Centre;
-    id: string;
-    code: string;
-    libelle: string;
-    idProvince: string;
-}
--- esu/dist/entity/service.d.ts
+++
@@ -1,9 +0,0 @@
-import mongoose, { Document } from "mongoose";
-import { Departement } from "./departement";
-export declare type ServiceDocument = Service & Document;
-export declare class Service {
-    code: string;
-    libelle: string;
-    departement: Departement[];
-}
-export declare const ServiceSchema: mongoose.Schema<Service, mongoose.Model<Service, any, any, any, any>, {}, {}, {}, {}, "type", Service>;

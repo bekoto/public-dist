ad7d1bcdb0f4d8e80f1d179a9b171fbcaa2b5211
Brandon Ekoto <be_developer@bitbucket.com>
2022-08-17 01:16:08 +0100

    ddd

--- taxe/dist/controller/sync.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { AppService } from 'src/app.service';
-import { SyncAdapter } from 'src/dto/sync-adapter';
-export declare class SyncController {
-    private appServ;
-    constructor(appServ: AppService);
-    syncTaxesParking(data: SyncAdapter): Promise<import("../entity/taxe").Taxe[]>;
-}
--- taxe/dist/dto/sync.dto-.d.ts
+++
@@ -1,7 +0,0 @@
-import { Taxe } from "src/entity/taxe";
-export declare class SyncDto {
-    syncAt: number;
-    items: Taxe[];
-    countItem: number;
-    device: string;
-}
